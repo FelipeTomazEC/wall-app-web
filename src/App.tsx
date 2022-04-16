f3bde4f44af67d1627fcd4979c0ea2384067ec60
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-16 13:17:55 -0300

    feat: add login route to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { theme } from './styles/theme';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login } from './pages/Login';
 
 export const App: React.FC = () => {
 
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   )
 }
