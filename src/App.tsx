7d86de3fe9bdcea10c652c9d59024af196513514
FelipeTomazEC <felipetomaz2206@gmail.com>
2022-04-16 12:46:21 -0300

    refactor: remove commented code

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import { ThemeProvider } from 'styled-components';
 import { GlobalStyle } from './styles/global';
 import { theme } from './styles/theme';
 
-// import { Container } from './styles';
-
 export const App: React.FC = () => {
 
   return (
